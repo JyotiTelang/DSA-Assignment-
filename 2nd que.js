1bb29fdb10dcf8993bc6443456b852539334921a
Jyoti Manohar Telang <110247617+JyotiTelang@users.noreply.github.com>
2022-11-09 18:48:21 +0530

    Create 2nd que.js

---
+++ 2nd que.js
@@ -0,0 +1,11 @@
+
+    
+let arrayrev = [9,8,6,4,2,3,1];
+ 
+    console.log("The Original Array: ");
+    console.log(arrayrev);
+ 
+    arrayrev.reverse();
+ 
+    console.log(" The Reversed Array: ");
+    console.log(arrayrev);        
